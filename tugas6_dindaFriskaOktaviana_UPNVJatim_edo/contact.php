7519f1a3bf8dd0eb5aaeae568e00ef22391022a7
dindafris <73064809+dindafris@users.noreply.github.com>
2022-10-05 16:56:21 +0700

    Add files via upload

---
+++ tugas6_dindaFriskaOktaviana_UPNVJatim_edo/contact.php
@@ -0,0 +1,48 @@
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p>Contact Me</p>
+      </div>
+
+      <div class="row mt-2">
+
+        <div class="col-md-6 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-map"></i>
+            <h3>My Address</h3>
+            <p>Gamping, Krian, Sidoarjo, Jawa Timur</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-share-alt"></i>
+            <h3>Social Profiles</h3>
+            <div class="social-links">
+              <a href="https://twitter.com/dindafris" class="twitter" target="_blank"><i class="bi bi-twitter"></i></a>
+              <a href="https://instagram.com/dindafris_" class="instagram"><i class="bi bi-instagram" target="_blank"></i></a>
+              <a href="https://www.linkedin.com/in/dinda-friska-ab4517235/" class="linkedin" target="_blank"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-envelope"></i>
+            <h3>Email Me</h3>
+            <p>20081010121@student.upnjatim.ac.id</p>
+          </div>
+        </div>
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-phone-call"></i>
+            <h3>Call Me</h3>
+            <p>089518808060</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Contact Section -->
---
+++ tugas6_dindaFriskaOktaviana_UPNVJatim_edo/study.php
@@ -0,0 +1,44 @@
+  <!-- ======= Resume Section ======= -->
+  <section id="study" class="study">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Study</h2>
+        <p>Check My Study</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <h3 class="study-title">Education</h3>
+          <div class="study-item">
+            <h4>SDN Rungkut Kidul III/612</h4>
+            <h5>2008 - 2014</h5>
+            <p><em>Jl. Zamhuri No.31A, Rungkut Kidul, Surabaya</em></p>
+            <p>Bersekolah di SDN Rungkut Kidul III/612 pada tahun 2008 - 2014 dan menempuh pendidikan sekolah dasar dari kelas 1 hingga kelas 6 pada SD tersebut.</p>
+          </div>
+          <div class="study-item">
+            <h4>SMPN 35 Surabaya</h4>
+            <h5>2014 - 2017</h5>
+            <p><em>Jl. Rungkut Asri No.22, Rungkut Kidul, Surabaya</em></p>
+            <p>Bersekolah di SMPN 35 Surabaya pada tahun 2014 - 2017 dan menempuh pendidikan sekolah menengah pertama dari kelas 7 hingga kelas 9 pada SMP tersebut.</p>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <br><br><br>
+          <div class="study-item">
+            <h4>SMKN 1 Surabaya</h4>
+            <h5>2017 - 2020</h5>
+            <p><em>Jl. Smea No.4, Wonokromo, Surabaya</em></p>
+            <p>Menempuh pendidikan sekolah menengah akhir pada SMKN 1 Surabaya selama 3 tahun dan mengambil jurusan Rekayasa Perangkat Lunak</p>
+          </div>
+          <div class="study-item">
+            <h4>UPN Veteran Jawa Timur</h4>
+            <h5>2020 - Sekarang</h5>
+            <p><em>Jl. Rungkut Madya No.1, Gunung Anyar, Surabaya</em></p>
+            <p>Saat ini tengah menjalani pendidikan S1 pada UPN Veteran Jawa Timur dan mengambil program studi Informatika.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Resume Section -->
